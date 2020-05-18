b7f805fa59144f1fb8642feb2e0fd38e9aa85475
xhh <303158131@qq.com>
2020-05-19 02:56:21 +0800

    调整完毕

--- src/settings.js
+++ src/settings.js
@@ -3,7 +3,7 @@ module.exports = {
   // 获取令牌时，请求头信息(Basic Base64.encode(client_id:client_secret))
   authorizationValue: 'Basic ZmViczoxMjM0NTY=',
   // 社交登录后台地址
-  socialLoginUrl: 'http://localhost:8301/auth/social/login',
+  socialLoginUrl: 'http://apicloud.febs.cc:8301/auth/social/login',
   // 请替换为您的实际地址
   pages: {
     // FEBS-Admin 控制台地址
@@ -11,7 +11,7 @@ module.exports = {
     // kibana 控制台地址
     kibanaUrl: 'http://localhost:5601',
     // nacos 控制台地址
-    nacosUrl: 'http://localhost:8001/nacos',
+    nacosUrl: 'http://121.43.145.142:8849/nacos',
     // skywalking地址
     skywalkingUrl: 'http://localhost:8080/',
     // 文档中心
